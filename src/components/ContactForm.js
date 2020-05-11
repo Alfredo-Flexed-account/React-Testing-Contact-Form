2c5b4e039e3f414cd6ade8dcb1502391613b93cb
alfredo villasenor <alfredo.villasenor.aof@gmail.com>
2020-05-11 12:23:09 -0700

    MVP met

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,10 +3,11 @@ import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
   const [data, setData] = useState();
+  const [numClicks, setNumClicks] = useState(0);
   const { register, errors, handleSubmit } = useForm({
     mode: "onBlur",
   });
-  const onSubmit = (data) => {
+  const onSubmit = data => {
     setData(data);
   };
 
@@ -16,6 +17,8 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id= "firstname"
+            type= 'text'
             name="firstName"
             placeholder="Edd"
             ref={register({ required: true, maxLength: 3 })}
@@ -28,6 +31,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+          id= "lastname"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -41,14 +45,14 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id= "email" name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea id= "message" name="message" ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
@@ -57,6 +61,11 @@ const ContactForm = () => {
         )}
         <input type="submit" />
       </form>
+      <button onClick={e=>{
+          e.preventDefault();
+          setNumClicks(numClicks+1);
+        }}>click me: {numClicks}</button>
+
     </div>
   );
 };
